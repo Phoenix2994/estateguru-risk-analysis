939321e728e29cf705a983a39ac00385692799ce
Phoenix2994 <filippobianco2@gmail.com>
2020-06-10 22:08:44 +0200

    0.0.1

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-    {
-      path: 'risk',
-      loadChildren: () => import('../risk/risk.module').then(m => m.RiskModule)
-    }
-  ];
-  
-  @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  })
-  export class CoreRoutingModule { }
+  {
+    path: 'risk',
+    loadChildren: '../risk/risk.module#RiskModule'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class CoreRoutingModule { }
